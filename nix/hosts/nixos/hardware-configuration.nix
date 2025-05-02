c3633a4a6df97e4d2598be8d50c2687aed675615
AdanW7 <adanwodzinski7@gmail.com>
2025-05-02 00:04:05 -0500

    added amdgpu to kernel

--- nix/hosts/nixos/hardware-configuration.nix
+++ nix/hosts/nixos/hardware-configuration.nix
@@ -9,15 +9,16 @@
          (modulesPath + "/installer/scan/not-detected.nix")
       ];
 
-   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-   boot.initrd.kernelModules = [ ];
-   boot.kernelModules = [ "kvm-amd" ];
-   boot.extraModulePackages = [ ];
-   # boot = {
-   #    initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-   #    initrd.kernelModules = [ ];
-   #    kernelModules = [ "kvm-amd" ];
-   # };
+   # boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+   # boot.initrd.kernelModules = [ ];
+   # boot.kernelModules = [ "kvm-amd" ];
+   # boot.extraModulePackages = [ ];
+   boot = {
+      initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+      initrd.kernelModules = ["amdgpu" ];
+      kernelModules = [ "kvm-amd" ];
+      extraModulePackages = [ ];
+   };
 
    fileSystems."/" ={ 
       device = "/dev/disk/by-uuid/78b5fffd-0f8f-459c-a798-8173e1c2dc39";
