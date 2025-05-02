f2ddd90c4ddcd73d930eb1ee3f2354cff2211cf2
AdanW7 <adanwodzinski7@gmail.com>
2025-05-02 12:20:55 -0500

    adjusted nixos home config

--- nix/hosts/nixos/home.nix
+++ nix/hosts/nixos/home.nix
@@ -40,6 +40,8 @@ in
          pkg-config
 
          steam
+         tmux
+         # tmux-sessionizer
 
       ];
 
@@ -85,17 +87,17 @@ in
          };
 
          shellAliases = lib.mkMerge [
-            (lib.mkIf isDarwin {
-               ll = "ls -l";
-               la = "ls -la";
-               # ndi = "nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --flake ~/.config/nix";
-               # ndr = "darwin-rebuild switch --flake ~/.config/nix#Adans-MacBook-Air";
-               ndr = "darwin-rebuild switch --flake ${homeDirectory}/.config/nix#Adans-MacBook-Air";
-               nixgc = "nix-collect-garbage -d && nix store optimise";
-               ndu = "nix flake update --flake ${homeDirectory}/.config/nix";
-               nix-c-dev = "nix develop github:AdanW7/nix_C_dev_flake --impure --no-write-lock-file --command $SHELL";
-
-            })
+            # (lib.mkIf isDarwin {
+            #    ll = "ls -l";
+            #    la = "ls -la";
+            #    # ndi = "nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --flake ~/.config/nix";
+            #    # ndr = "darwin-rebuild switch --flake ~/.config/nix#Adans-MacBook-Air";
+            #    ndr = "darwin-rebuild switch --flake ${homeDirectory}/.config/nix#Adans-MacBook-Air";
+            #    nixgc = "nix-collect-garbage -d && nix store optimise";
+            #    ndu = "nix flake update --flake ${homeDirectory}/.config/nix";
+            #    nix-c-dev = "nix develop github:AdanW7/nix_C_dev_flake --impure --no-write-lock-file --command $SHELL";
+            #
+            # })
             (lib.mkIf isLinux {
                ll = "ls -l";
                la = "ls -la";
@@ -110,36 +112,36 @@ in
          initContent  = lib.mkMerge [
 
             # Mac-specific init
-            (lib.mkIf isDarwin ''
-              # Exports
-              # export PATH="$PATH:/Users/adan/.local/bin"
-              export PATH="$PATH:/Users/${username}/.local/bin"
-              export PATH="$PATH:/opt/homebrew/bin"
-              export PKG_CONFIG_PATH="$(brew --prefix)/lib/pkgconfig:$PKG_CONFIG_PATH"
-
-
-              # Zsh plugins
-              source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-              source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-              # >>> conda initialize >>>
-              # !! Contents within this block are managed by 'conda init' !!
-              # __conda_setup="$('/Users/adan/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-              __conda_setup="$('/Users/${username}/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-              if [ $? -eq 0 ]; then
-                  eval "$__conda_setup"
-              else
-                  # if [ -f "/Users/adan/miniconda3/etc/profile.d/conda.sh" ]; then
-                  if [ -f "/Users/${username}/miniconda3/etc/profile.d/conda.sh" ]; then
-                      # . "/Users/adan/miniconda3/etc/profile.d/conda.sh"
-                      . "/Users/${username}/miniconda3/etc/profile.d/conda.sh"
-                  else
-                      # export PATH="/Users/adan/miniconda3/bin:$PATH"
-                      export PATH="/Users/${username}/miniconda3/bin:$PATH"
-                  fi
-              fi
-              unset __conda_setup
-            '')
+            # (lib.mkIf isDarwin ''
+            #   # Exports
+            #   # export PATH="$PATH:/Users/adan/.local/bin"
+            #   export PATH="$PATH:/Users/${username}/.local/bin"
+            #   export PATH="$PATH:/opt/homebrew/bin"
+            #   export PKG_CONFIG_PATH="$(brew --prefix)/lib/pkgconfig:$PKG_CONFIG_PATH"
+            #
+            #
+            #   # Zsh plugins
+            #   source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+            #   source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+            #
+            #   # >>> conda initialize >>>
+            #   # !! Contents within this block are managed by 'conda init' !!
+            #   # __conda_setup="$('/Users/adan/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+            #   __conda_setup="$('/Users/${username}/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+            #   if [ $? -eq 0 ]; then
+            #       eval "$__conda_setup"
+            #   else
+            #       # if [ -f "/Users/adan/miniconda3/etc/profile.d/conda.sh" ]; then
+            #       if [ -f "/Users/${username}/miniconda3/etc/profile.d/conda.sh" ]; then
+            #           # . "/Users/adan/miniconda3/etc/profile.d/conda.sh"
+            #           . "/Users/${username}/miniconda3/etc/profile.d/conda.sh"
+            #       else
+            #           # export PATH="/Users/adan/miniconda3/bin:$PATH"
+            #           export PATH="/Users/${username}/miniconda3/bin:$PATH"
+            #       fi
+            #   fi
+            #   unset __conda_setup
+            # '')
 
             # Linux-specific init
             (lib.mkIf isLinux ''
