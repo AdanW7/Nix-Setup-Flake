c9c9804a45888283cce3843cce82681f0de54e5c
AdanW7 <adanwodzinski7@gmail.com>
2025-05-09 01:54:34 -0500

    disabled wireless

--- nix/hosts/nixos/configuration.nix
+++ nix/hosts/nixos/configuration.nix
@@ -27,7 +27,7 @@ in
 
    networking = {
       hostName = "nixos";
-      wireless.enable = true;
+      # wireless.enable = true;
       proxy.default = "http://user:password@proxy:port/";
       proxy.noProxy = "127.0.0.1,localhost,internal.domain";
       networkmanager.enable = true;
