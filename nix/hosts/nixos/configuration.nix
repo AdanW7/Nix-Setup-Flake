a104be06f03f432d2c322b8ead958952e157a1dd
AdanW7 <adanwodzinski7@gmail.com>
2025-05-09 00:17:18 -0500

    added killall

--- nix/hosts/nixos/configuration.nix
+++ nix/hosts/nixos/configuration.nix
@@ -73,6 +73,7 @@ in
    # List packages installed in system profile. To search, run:
    # $ nix search wget
    environment.systemPackages = with pkgs; [
+      killall
       zsh
       wget
       curl
