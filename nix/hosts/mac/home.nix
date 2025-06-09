0cacb80349166434a2940bb391646df1ac98c284
Adan Wodzinski <Adanwodzinski7@gmail.com>
2025-06-09 17:03:48 -0500

    removed a function y() yazi setup since it was incorrect

--- nix/hosts/mac/home.nix
+++ nix/hosts/mac/home.nix
@@ -155,14 +155,6 @@ in
                  fi
                  unset __conda_setup
 
-               #yazi setup
-               function y() {
-                  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-                  yazi "$@" --cwd-file="$tmp"
-                  IFS= read -r -d '''' cwd < "$tmp"
-                  [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
-                  rm -f -- "$tmp"
-               }
             '')
 
          ];
